41d56c817cc31c3d0c3ad74a5f8b5ec94d1cac17
Misha Tsap <tsapmisha@gmail.com>
2018-08-09 01:49:53 +0300

    fulfilled

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function Math(firstArg, secondArg, thirdArg) {
+	return (firstArg + secondArg * thirdArg)
+};
+console.log(Math(53, 61, 67));
---
+++ number-to-string.js
@@ -0,0 +1,5 @@
+// var n = 128;
+// var numToString = n = n.toString();
+// console.log(numToString);
+ var n = 128;
+ console.log(n = n.toString());
